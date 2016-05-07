93ba2c376fb4f2ae4ce7905e23a07a54e7dfd4b9
Jeremy Othieno <j.othieno@gmail.com>
2016-05-07 03:01:27 +0200

    Upgrade the geotagx-sanitizer version

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ setup(
 	zip_safe=True,
 	install_requires=[
 		"geotagx_builder",
-		"geotagx_sanitizer>=0.0.3",
+		"geotagx_sanitizer>=0.0.5",
 	],
 	dependency_links=[
-		"https://github.com/othieno/geotagx-tool-sanitizer/archive/v0.0.3.tar.gz#egg=geotagx_sanitizer-0.0.3",
+		"https://github.com/othieno/geotagx-tool-sanitizer/archive/v0.0.5.tar.gz#egg=geotagx_sanitizer-0.0.5",
 	],
 	# keywords="",
 	# author="",
